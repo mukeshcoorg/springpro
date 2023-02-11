67284ab6cb9a60fd2fe6252d45d521f321ea1829
mukeshcoorg <mukeshgowdahb@gmail.com>
2023-02-11 11:24:12 +0530

    i commit

---
+++ src/main/java/com/example/spring/progect/SpringProgectApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.progect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProgectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProgectApplication.class, args);
+	}
+
+}
